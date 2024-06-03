b282657654947ba16a44e6ba350fae01971b0cbe
DevrimCaner <devrimcanerokdem@outlook.com>
2024-06-03 16:48:26 +0300

    Projects

--- devrimcaner/src/components/Heading.tsx
+++ devrimcaner/src/components/Heading.tsx
@@ -5,7 +5,7 @@ const Heading: React.FC = () => {
   return (
     <Container>
       <Grid container spacing={2}>
-        <Grid item xs={12} md>
+        <Grid item xs={12} md={4}>
           <Box className="box">
             <Typography variant="h3" align="center">Section 1</Typography>
             <Typography variant="body1" align="center">This is the content of section 1.</Typography>
@@ -13,7 +13,7 @@ const Heading: React.FC = () => {
 
           </Box>
         </Grid>
-        <Grid item xs={12} md>
+        <Grid item xs={12} md={4}>
           <Box className="box">
             <Typography variant="h3" align="center">Section 2</Typography>
             <Typography variant="body1" align="center">This is the content of section 2.</Typography>
@@ -21,7 +21,7 @@ const Heading: React.FC = () => {
 
           </Box>
         </Grid>
-        <Grid item xs={12} md>
+        <Grid item xs={12} md={4}>
           <Box className="box">
             <Typography variant="h3" align="center">Section 3</Typography>
             <Typography variant="body1" align="center">This is the content of section 3.</Typography>
--- devrimcaner/src/pages/projects.tsx
+++ devrimcaner/src/pages/projects.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import Link from 'next/link';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Grid, Box } from '@mui/material';
+import ProjectCard from '@/components/ProjectCard';
+
+const projects = [0,1,2,3,4,5];
+const sampleData = {
+  image: 'https://placehold.co/600x400',
+  title: 'Project Name',
+  date: 'February 1, 2024',
+  categories:['C#','C++', 'Unity'],
+  description: 'This is a short description of Project 2.',
+};
 
 const Projects: React.FC = () => {
   return (
-    <Container>
-      <Typography variant="h2" component="h1" gutterBottom>
-        Projects Page
-      </Typography>
-      <Typography variant="body1" paragraph>
-        This is the projects page.
-      </Typography>
+    <Container style={{marginTop:50, marginBottom:50}}>
+      <Grid container spacing={2}>
+      
+      {projects.map((project, index) => (
+        <Grid item xs={12}>
+          <ProjectCard {...sampleData}/>
+        </Grid>
+      ))}
+
+      </Grid>
     </Container>
   );
 }
