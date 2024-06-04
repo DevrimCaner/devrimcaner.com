28dba9736e75230036ce52a6bf0efc18644d7854
DevrimCaner <devrimcanerokdem@outlook.com>
2024-06-04 17:02:17 +0300

    Navbar Mobile

--- devrimcaner/src/components/Navbar.tsx
+++ devrimcaner/src/components/Navbar.tsx
@@ -1,36 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Box, Drawer, IconButton, List, ListItem, ListItemText } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const pages = [{ name: 'Home', path: '/' }, { name: 'Projects', path: '/projects' }, { name: 'Blog', path: '/blog' }];
 
 const Navbar: React.FC = () => {
-  return (
-    <AppBar
-      position="sticky"
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+    if (event.type === 'keydown' && ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const list = () => (
+    <Box
       sx={{
-        background: 'rgba(0, 0, 0, 0.6)',
-        backdropFilter: 'blur(10px)',
-        boxShadow: 'none',
-      }}
+        width: 250
+       }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
     >
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
-          <Link href="/" passHref legacyBehavior>
-            <a style={{ color: 'white', textDecoration: 'none' }}>Logo</a>
-          </Link>
-        </Typography>
-        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-          {pages.map((page) => (
-            <Link key={page.name} href={page.path} passHref legacyBehavior>
-              <Button sx={{ color: 'white' }}>
-                {page.name}
-              </Button>
+      <List >
+        {pages.map((page) => (
+          <ListItem button key={page.name}>
+            <Link href={page.path} passHref legacyBehavior>
+              <ListItemText primary={page.name} />
             </Link>
-          ))}
-        </Box>
-      </Toolbar>
-    </AppBar>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <AppBar
+        position="sticky"
+        sx={{
+          background: 'rgba(0, 0, 0, 0.6)',
+          backdropFilter: 'blur(10px)',
+          boxShadow: 'none',
+        }}
+      >
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
+            <Link href="/" passHref legacyBehavior>
+              <a style={{ color: 'white', textDecoration: 'none' }}>Logo</a>
+            </Link>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Link key={page.name} href={page.path} passHref legacyBehavior>
+                <Button sx={{ color: 'white' }}>
+                  {page.name}
+                </Button>
+              </Link>
+            ))}
+          </Box>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        style={{
+        background: 'rgba(0, 0, 0, 0.1)',
+        backdropFilter: 'blur(5px)',
+        boxShadow: 'none',
+       }}
+        anchor="left"
+        open={drawerOpen}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          sx: {
+            backgroundColor: '#ecf0f1'
+          },
+        }}
+      >
+        {list()}
+      </Drawer>
+    </>
   );
 }
 
