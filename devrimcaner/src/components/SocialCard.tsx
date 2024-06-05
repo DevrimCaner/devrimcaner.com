48088c2f1366e534f898ad4097f45d6b37037a54
DevrimCaner <devrimcanerokdem@outlook.com>
2024-06-05 16:38:52 +0300

    Social Temp style update

--- devrimcaner/src/components/SocialCard.tsx
+++ devrimcaner/src/components/SocialCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardMedia, Typography, Box, IconButton } from '@mui/material';
+import { Card, CardContent, CardMedia, Typography, Box, Grid } from '@mui/material';
 
 interface SocialCardProps {
   icon: React.ReactNode;
@@ -14,7 +14,7 @@ const SocialCard: React.FC<SocialCardProps> = ({ icon, title, description, link,
     <Card
       sx={{
         transition:'all 0.2s',
-        height: '100px',
+        height: 'auto',
         borderRadius: '16px',
         borderWidth: '2px',
         borderStyle: 'solid',
@@ -31,19 +31,33 @@ const SocialCard: React.FC<SocialCardProps> = ({ icon, title, description, link,
       }}
       onClick={() => window.open(link, '_blank')}
     >
-      <CardMedia sx={{ marginRight: '16px' }}>
-        <IconButton>
-          {icon}
-        </IconButton>
-      </CardMedia>
-      <CardContent sx={{ padding: '0 !important' }}>
-        <Typography variant="h6" component="div" >
-          {title}
-        </Typography>
-        <Typography variant="body2">
-          {description}
-        </Typography>
-      </CardContent>
+        <Grid container>
+            <Grid item xs={12}>
+                <CardMedia sx={{ marginRight: '16px'}}>
+                    <Box sx={{
+                        backgroundColor: color,
+                        width: '40px',
+                        height: '40px',
+                        borderRadius: '12px',
+                        textAlign: 'center',
+                        justifyContent: 'center',
+                        paddingTop:'8px'
+                    }}>
+                    {icon}
+                    </Box>
+                </CardMedia>
+            </Grid>
+            <Grid item xs={12}>
+                <CardContent sx={{ padding: '0 !important' }}>
+                    <Typography variant="h6" component="div" >
+                    {title}
+                    </Typography>
+                    <Typography variant="body2" style={{opacity:0.6}}>
+                    {description}
+                    </Typography>
+                </CardContent>
+            </Grid>
+        </Grid>
     </Card>
   );
 };
--- devrimcaner/src/components/Socials.tsx
+++ devrimcaner/src/components/Socials.tsx
@@ -7,42 +7,42 @@ import { Description } from '@mui/icons-material';
 const elemets = [0,1,2,3,4,5,6,7,8,9];
 const socials = [
     {
-        icon: <Facebook style={{color:'#3b5998'}}/>,
+        icon: <Facebook style={{color:'#fff'}}/>,
         title: 'Facebook',
         description: 'Follow us on Facebook',
         link: 'https://www.facebook.com/',
         color: '#3b5998',
       },
       {
-        icon: <Twitter style={{color:'#1da1f2'}}/>,
+        icon: <Twitter style={{color:'#fff'}}/>,
         title: 'Twitter',
         description: 'Follow us on Twitter',
         link: 'https://www.twitter.com/',
         color: '#1da1f2',
       },
       {
-        icon: <Instagram style={{color:'#e4405f'}}/>,
+        icon: <Instagram style={{color:'#fff'}}/>,
         title: 'Instagram',
         description: 'Follow us on Instagram',
         link: 'https://www.instagram.com/',
         color: '#e4405f',
       },
       {
-        icon: <YouTube style={{color:'#ff0000'}}/>,
+        icon: <YouTube style={{color:'#fff'}}/>,
         title: 'Youtube',
         description: 'Follow us on Youtube',
         link: 'https://www.instagram.com/',
         color: '#ff0000',
       },
       {
-        icon: <GitHub style={{color:'#171515'}}/>,
+        icon: <GitHub style={{color:'#fff'}}/>,
         title: 'GitHub',
         description: 'Follow us on Youtube',
         link: 'https://www.instagram.com/',
         color: '#171515',
       },
       {
-        icon: <LinkedIn style={{color:'#0077b5'}}/>,
+        icon: <LinkedIn style={{color:'#fff'}}/>,
         title: 'LinkedIn',
         description: 'Follow us on Youtube',
         link: 'https://www.instagram.com/',
